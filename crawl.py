f69be294910ca0f537be047aaa59c1967886b349
Sam Zhou <jzhou104@syr.edu>
2019-02-21 16:39:04 -0500

    update

--- crawl.py
+++ crawl.py
@@ -18,13 +18,13 @@ except:
 
 
 def getScreenName(users):
-    str = ""
+    output = ""
     p = twitter_network.get_user_profile(twitter_api, screen_names=None, user_ids=users)
     for user in p:
-        str += (p[user]['name']) + ", "
-    str = ''.join(str)[:-2]
+        output += (p[user]['name']) + "(" + str(int((p[user]['followers_count']))) + "), "
+    output = ''.join(output)[:-2]
 
-    return str
+    return output
 
 
 def pickFiveMostPopular(users):
@@ -35,9 +35,8 @@ def pickFiveMostPopular(users):
         unsortedList_by_follower_count.append(tuple((user, p[user]['followers_count'])))
     sortedList_by_follower_count = sorted(unsortedList_by_follower_count, key = lambda x : x[1], reverse=True)
     top5 = [x[0] for x in sortedList_by_follower_count[:5]]
-    print("\nThe five most popular people who follow " + screen_name + " are: " + getScreenName(top5))
+    print("The five most popular people who follow " + screen_name + " are: " + getScreenName(top5))
 
     return top5
 
-
-pickFiveMostPopular(reciprocal_friends_ids)
+network_main_nodes = pickFiveMostPopular(reciprocal_friends_ids)
